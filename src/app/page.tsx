bf638d16bafffa44878a66a66e39fa54c598b678
Majid Ali <160537411+Majidali80@users.noreply.github.com>
2024-12-26 09:50:22 +0500

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,25 @@
+import Topheader from "./components/Topheader";
+import Hero from "./components/Hero";
+import Editors from "./components/editorspick";
+import Classman from "./components/Classman";
+import Navbar from "./components/navbar";
+import ProductCard from "./components/products-card";
+import Univiverse from "./components/Unviverse";
+import FuturePost from "./components/FuturePost";
+import Footer from "./components/footer";
+
+export default function Home() {
+  return (
+   <> 
+
+      <Topheader />
+      <Navbar />
+      <Hero />
+      <Editors />
+      <ProductCard />
+      <Classman />git remote add origin https://github.com/Majidali80/Bandage.git
+       <FuturePost />
+       <Footer />
+       </>
+  );
+}
